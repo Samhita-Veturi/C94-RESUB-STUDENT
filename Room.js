2e73821fc5fb7f92d6c9af8a465b41440b2aecf2
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-01-22 23:36:39 +0530

    Add files via upload

---
+++ Room.js
@@ -0,0 +1,30 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBgE6YBmHl3TmnzJPV44OjKkMTLd9A1ttE",
+  authDomain: "c-95-project.firebaseapp.com",
+  databaseURL: "https://c-95-project-default-rtdb.firebaseio.com",
+  projectId: "c-95-project",
+  storageBucket: "c-95-project.appspot.com",
+  messagingSenderId: "512265434057",
+  appId: "1:512265434057:web:3e4ec92612fdd78a6eee18"
+};
+// Initialize Firebase
+
+var UserName_1 = localStorage.getItem("User_1");
+document.getElementById("UserName_Display").innerHTML = "Welcome " + UserName_1;
+
+firebase.initializeApp(firebaseConfig);
+
+function AddRoom(){
+    Add_User = document.getElementById("Input_Room_Name").value;
+    console.log(Add_User);
+    firebase.database().ref("/").child(Add_User).update({
+        Purpose: "Adding Room"
+    });
+    document.getElementById("Output").innerHTML += Add_User;
+}
+function LogOut(){
+  localStorage.removeItem("User_1");
+  localStorage.removeItem("Room_Name");
+  window.location = "Login.html";
+}
